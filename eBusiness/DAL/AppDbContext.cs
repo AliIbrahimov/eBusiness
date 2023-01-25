30a9778c9dd2136adc7e7c775148156a9ccf8847
Aliibrahimov <aliibrah30@gmail.com>
2023-01-25 17:02:53 +0400

    added

---
+++ eBusiness/DAL/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using eBusiness.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eBusiness.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options){}
+        public DbSet<Setting> Settings { get; set; }
+        public DbSet<Icon> Icons { get; set; }
+        public DbSet<Member> Members { get; set; }
+    }
+}
